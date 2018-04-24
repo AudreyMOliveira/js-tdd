ad46dc9d5b47661385c1351552b073e4e4ca8f3f
ASS_TI_322\Audrey.Oliveira <audrey.oliveira@assertivasolucoes.com.br>
2018-04-24 14:30:24 -0300

    adding husky

---
+++ src/main.js
@@ -0,0 +1,9 @@
+const a = 3;
+let b = 2;
+b = 4;
+const c = 'OIHSODHSAODSHDOAsdofhfoihofihdsofidhsfosihfsdoifhsdofihfodsihfsoifhsdofihsdfoidshfoi';
+
+function sum() {
+  return a + b + c;
+}
+sum();
